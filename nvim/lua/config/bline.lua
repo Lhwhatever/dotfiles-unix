eed5d956179d5bdf1983e739e74ad94458455673
Lhwhatever <llinhong.2301+github@gmail.com>
2021-09-08 23:19:28 -0400

    nvim: Update bline.lua for new version of bufferline

--- nvim/lua/config/bline.lua
+++ nvim/lua/config/bline.lua
@@ -3,7 +3,6 @@ local keymaps = require 'keymaps'
 require 'bufferline'.setup {
     options = {
         numbers = 'ordinal',
-        mappings = true,
         diagnostics = 'nvim_lsp',
         show_buffer_close_icons = false,
         separator_style = { ' ', ' ' },
