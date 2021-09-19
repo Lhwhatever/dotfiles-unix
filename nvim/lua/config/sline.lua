5da4072832ebf5eb6b349b5702de7636fdd54198
Lhwhatever <llinhong.2301+github@gmail.com>
2021-09-19 01:07:04 -0400

    nvim: Update sline.lua for new version of feline
    Now makes use of the winid feature as the current buffer (in the context
    of evaluating the statusline) is now the current selected buffer instead
    of the buffer of the attached window, which breaks the previous
    implementation of sline.lua.

--- nvim/lua/config/sline.lua
+++ nvim/lua/config/sline.lua
@@ -194,9 +194,37 @@ local mode_component = {
     right_sep = function() return { str = 'right_filled', hl = primary_inner_hl() } end,
 }
 
+local function get_gsd(winid)
+    return pcall(vim.api.nvim_buf_get_var, vim.api.nvim_win_get_buf(winid), 'gitsigns_status_dict')
+end
+
+
+local function make_git_branch_component(active) return {
+    provider = function(_, winid)
+        local ok, gsd = get_gsd(winid)
+
+        if not ok then
+            return ''
+        end
+
+        return '  ' .. gsd.head
+    end,
+    hl = ternary(active, secondary_hl, inactive_hl),
+    enabled = function(winid)
+        local ok, _ = get_gsd(winid)
+        return ok
+    end
+} end
+
+
 local function make_git_component(active) return {
-    provider = function()
-        local gsd = vim.b.gitsigns_status_dict
+    provider = function(_, winid)
+        local ok, gsd = get_gsd(winid)
+
+        if not ok then
+            return ''
+        end
+
         local output = ' '
         if gsd['added'] and gsd['added'] > 0 then
             output = output .. ' ' .. gsd['added'] .. ' '
@@ -210,18 +238,10 @@ local function make_git_component(active) return {
         return output
     end,
     hl = ternary(active, tertiary_hl, inactive_hl),
-    enabled = function() return vim.b.gitsigns_status_dict ~= nil end,
-    left_sep = ternary(
-        active,
-        function() return {
-            str = '  ' .. vim.b.gitsigns_status_dict.head,
-            hl =  secondary_hl()
-        } end,
-        function() return {
-            str = '  ' .. vim.b.gitsigns_status_dict.head,
-            hl = inactive_hl
-        } end
-    ),
+    enabled = function(winid)
+        local ok, _ = get_gsd(winid)
+        return ok
+    end,
     right_sep = ternary(
         active,
         function() return { str = 'right', hl = secondary_hl() } end,
@@ -229,30 +249,57 @@ local function make_git_component(active) return {
     )
 } end
 
-local function get_file_icon()
-    local name = vim.fn.expand('%')
-    local ext = vim.fn.expand('%:e')
-    return ' ' .. icons.get_icon(name, ext, { default = true }) .. ' '
+local function get_fname(winid)
+    return vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(winid))
 end
 
-local function get_file_hl_active() return {
+local modified_hl_active = {
     bg = 'bg2',
-    fg = ternary(vim.bo.modified, 'orange', get_vi_color())
-} end
+    fg = 'orange',
+}
 
-local function get_file_hl_inactive() return {
+local modified_hl_inactive = {
     bg = 'bg0',
-    fg = ternary(vim.bo.modified, 'orange', 'fg0')
-} end
+    fg = 'orange',
+}
+
+
+local function make_file_icon_component(active, modified)
+    return {
+        provider = function(_, winid)
+            if is_inactive_file() then
+                return ' '
+            end
+
+            local name = get_fname(winid)
+            local ext = vim.fn.fnamemodify(name, ':e')
+            return ' ' .. icons.get_icon(name, ext, { default = true }) .. ' '
+        end,
+        hl = ternary(
+            modified,
+            ternary(active, modified_hl_active, modified_hl_inactive),
+            ternary(active, secondary_hl, inactive_hl)
+        ),
+        enabled = ternary(
+            modified,
+            function (winid)
+                return vim.bo[vim.api.nvim_win_get_buf(winid)].modified
+            end,
+            function (winid)
+                return not vim.bo[vim.api.nvim_win_get_buf(winid)].modified
+            end
+        )
+    }
+end
 
 local function make_directory_component(active)
     return {
-        provider = function()
+        provider = function(_, winid)
             if is_inactive_file() then
                 return ''
             end
 
-            local filename = vim.fn.bufname()
+            local filename = get_fname(winid)
             filename = vim.fn.pathshorten(vim.fn.fnamemodify(filename, ':~:.'))
 
             local dirname = vim.fn.fnamemodify(filename, ':h')
@@ -266,31 +313,40 @@ local function make_directory_component(active)
             active,
             {fg = 'lgrey', bg = 'bg2'},
             {fg = 'xgrey', bg = 'bg0'}
-        ),
-        left_sep = ternary(
-            active,
-            function() return {
-                str = get_file_icon(),
-                hl = get_file_hl_active()
-            } end,
-            function() return {
-                str = get_file_icon(),
-                hl = get_file_hl_inactive()
-            } end
         )
     }
 end
 
-local function make_filename_component(active)
+local function make_filename_component(active, modified)
     return {
-        provider = function()
-            local filename = vim.fn.expand('%:t')
-            local tags = ' '
-            if vim.bo.modified then tags = tags .. '●' end
-            if vim.bo.readonly then tags = tags .. '' end
-            return filename .. tags .. ' '
-        end,
-        hl = ternary(active, get_file_hl_active, get_file_hl_inactive),
+        provider = ternary(
+            modified,
+            function(_, winid)
+                local filename = vim.fn.fnamemodify(get_fname(winid), ':t')
+                local tags = ' ●'
+                return filename .. tags .. ' '
+            end,
+            function(_, winid)
+                local filename = vim.fn.fnamemodify(get_fname(winid), ':t')
+                local tags = ' '
+                if vim.bo[vim.api.nvim_win_get_buf(winid)].readonly then tags = tags .. '' end
+                return filename .. tags .. ' '
+            end
+        ),
+        hl = ternary(
+            modified,
+            ternary(active, modified_hl_active, modified_hl_inactive),
+            ternary(active, secondary_hl, inactive_hl)
+        ),
+        enabled = ternary(
+            modified,
+            function (winid)
+                return vim.bo[vim.api.nvim_win_get_buf(winid)].modified
+            end,
+            function (winid)
+                return not vim.bo[vim.api.nvim_win_get_buf(winid)].modified
+            end
+        ),
         right_sep = 'right_filled'
     }
 end
@@ -345,18 +401,16 @@ local function make_diag_component(type, active) return {
     provider = 'diagnostic_' .. diag_constr.provider[type],
     enabled = function() return lsp.diagnostics_exist(diag_constr.condition[type]) end,
     hl = {
-        fg = ternary(active,diag_constr.fg[type], 'fg0'),
+        fg = ternary(active, diag_constr.fg[type], 'fg0'),
         bg = ternary(active, 'bg2', 'bg0'),
-    }
+    },
 } end
 
 
 local secondary_left = {
-    provider = '',
-    left_sep = {
-        str = 'left_filled',
-        hl = { fg = 'bg2', bg = 'NONE' },
-    }
+    provider = ' ',
+    hl = secondary_hl,
+    left_sep = 'left_filled',
 }
 
 local inactive_left = {
@@ -366,14 +420,13 @@ local inactive_left = {
 }
 
 local inactive_right = {
-    provider = '',
-    hl = { bg = 'NONE' },
+    provider = ' ',
+    hl = inactive_hl,
     left_sep = {
         str = 'left_filled',
         hl = { fg = 'bg0' },
     },
 }
-
 
 local InactiveStatusHL = {
     --fg = vim.api.nvim_exec('hi VertSplit', true):match('guifg=(#%x+)') or '#444444',
@@ -394,9 +447,13 @@ local components = {
     active = {
         {
             mode_component,
+            make_git_branch_component(true),
             make_git_component(true),
+            make_file_icon_component(true, true),
+            make_file_icon_component(true, false),
             make_directory_component(true),
-            make_filename_component(true),
+            make_filename_component(true, true),
+            make_filename_component(true, false),
             empty_component
         },
         {},
@@ -414,9 +471,13 @@ local components = {
     inactive = {
         {
             inactive_left,
+            make_git_branch_component(false),
             make_git_component(false),
+            make_file_icon_component(false, true),
+            make_file_icon_component(false, false),
             make_directory_component(false),
-            make_filename_component(false),
+            make_filename_component(false, true),
+            make_filename_component(false, false),
             empty_component,
         },
         {
