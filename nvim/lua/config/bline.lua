e6acd88595fcc545f2f5295769780a77ea969c70
Lin Hong <llinhong.2301+github@gmail.com>
2022-01-19 00:26:07 -0500

    (nvim) Fix bufferline not being colored
    Somehow you need vim.cmd('colorscheme <name>') before the require
    'bufferline'.setup invocation

--- nvim/lua/config/bline.lua
+++ nvim/lua/config/bline.lua
@@ -1,12 +1,14 @@
 local keymaps = require 'keymaps'
 
+vim.cmd('colorscheme sonokai')
+
 require 'bufferline'.setup {
     options = {
         numbers = 'ordinal',
         diagnostics = 'nvim_lsp',
         show_buffer_close_icons = false,
         separator_style = { ' ', ' ' },
-    }
+    },
 }
 
 keymaps.map({
