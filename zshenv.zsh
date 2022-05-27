051b554fe0a56ecd4bfd9cb045b816f5fb0b6ee1
Lhwhatever <llinhong.2301+github@gmail.com>
2022-05-27 11:39:40 -0400

    (zsh) General updates

--- zshenv.zsh
+++ zshenv.zsh
@@ -3,17 +3,16 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # Setup neovim virtualenv
-export NVIM_PYTHON="~/neovim/venv/bin/python3"
+export NVIM_PYTHON="/usr/share/nvim/pyenv/bin/python3"
 
 # Setup debugpy virtualenv
 # export DEBUGPY_PYTHON="~/lib/python3/.virtualenvs/debugpy/bin/python3"
 
 # Setup PATH
-export PATH="$PATH:$(yarn global bin)"
+export PATH="$PATH:$(yarn global bin):~/.local/bin"
 
 # Setup EDITOR and VISUAL
 export VISUAL=nvim
 export EDITOR=$VISUAL
-export LC_TIME=en_SI.UTF_8
-export LC_ALL=en_SG.UTF_8
-. "$HOME/.cargo/env"
+
+[ -s "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
--- zshrc.zsh
+++ zshrc.zsh
@@ -127,3 +127,7 @@ unset __conda_setup
 # <<< conda initialize <<<
 
 # !! Contents within this block are managed by 'conda init' !!
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
