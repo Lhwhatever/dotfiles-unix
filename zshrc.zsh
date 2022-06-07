c25e3c20f598a1e6314240b70fdf0afc0b620d94
Lhwhatever <llinhong.2301+github@gmail.com>
2022-06-07 14:45:27 -0400

    (zsh) Change to use miniconda

--- zshrc.zsh
+++ zshrc.zsh
@@ -113,14 +113,14 @@ source $ZSH/oh-my-zsh.sh
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/lh/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('/home/lh/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/home/lh/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/lh/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "/home/lh/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/lh/miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/home/lh/anaconda3/bin:$PATH"
+        export PATH="/home/lh/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
